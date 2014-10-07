e944c68601955778fedb79192056e9ddc7fe69f8
Devin Jacobs <dbuttonz@gmail.com>
2014-10-07 12:57:53 -0600

    Build script. Problem 1 complete.

---
+++ src/Problem1.hs
@@ -0,0 +1,10 @@
+module Problem1 where
+
+
+main :: IO ()
+main = print $ sum [x | x <- [1..1000-1],
+                        x `divisibleBy` 3 || x `divisibleBy` 5]
+
+
+divisibleBy :: Integral n => n -> n -> Bool
+n `divisibleBy` d = n `mod` d == 0
